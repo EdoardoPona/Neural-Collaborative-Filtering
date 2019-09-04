ea0381bdf1c6313c7ede8c6974b956fae1dacf12
Edoardo Pona <edoardo.pona@gmail.com>
2019-09-04 22:26:23 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -6,17 +6,25 @@ import NCF
 import random
 import time
 import movielens_loader
+import numpy as np
 
 
 dataset_loader = movielens_loader.MovielensDatasetLoader()
-user2items, item2users = dataset_loader.build_dictionaries()
+
+user2items, item2users, test_user2items = dataset_loader.build_dictionaries()
 user_num, item_num = dataset_loader.user_num, dataset_loader.item_num
 
 
 def get_pos_user():
+    """ removes the element every time, so that we know what data we have trained the model on """
     user = random.choice(list(user2items.keys()))
-    item = random.choice(list(user2items[user].keys()))
-    return [int(user), int(item)]
+    items = list(user2items[user].keys())
+    i = random.randint(0, len(items)-1)
+    # item = random.choice(list(user2items[user].keys()))
+    del user2items[user][items[i]]
+    if len(user2items[user]) == 0:
+        del user2items[user]
+    return [int(user), int(items[i])]
 
 
 def get_neg_user():
@@ -35,10 +43,32 @@ def get_batch(size=128):
     return torch.Tensor(pos+neg).long().cuda(), target.cuda()
 
 
-def train(mode, optimizer):
+def HitRatio(test_num=100):
+    global test_user2items
+    hits = 0
+    test_users = list(test_user2items.keys())[:test_num]
+    for user in test_users:
+        x = [get_neg_user() for i in range(99)]
+        x.append([int(user), int(test_user2items[user])])
+        x_id = len(x)-1         # index of the last item, the positive one
+        x = torch.Tensor(x).long().cuda()
+        y = ncf(x, mode='ncf').squeeze(1).cpu().detach().numpy()
+        sort_ids = np.argsort(y)
+
+        if x_id in sort_ids[-10:]:
+            hits += 1
+    return hits / test_num
+
+
+def train(mode, optimizer, epochs=10, batch_size=128):
     # TODO use early stopping
-    for i in range(int(1e5)):
-        x, y = get_batch()
+    global user2items
+    global item2users
+    global test_user2items
+    ep = 0
+    i = 0
+    while ep < epochs:
+        x, y = get_batch(128)
         y_ = ncf(x, mode=mode)
         loss = F.binary_cross_entropy(y_, y)
         loss.backward()
@@ -46,10 +76,21 @@ def train(mode, optimizer):
         optimizer.zero_grad()
 
         if i % 100 == 0:
-            print('step', i, 'loss', loss.item())
+            print('epoch', ep, 'step', i, 'loss', loss.item())
+        i += 1
+
+        if i % 5000 == 0:
+            print('hit ratio', HitRatio())
+
+        if len(user2items) < batch_size:    # really there might be more left, but this is the minimum guaranteed
+            ep += 1
+            # user2items, item2users = dataset_loader.build_dictionaries()
+            user2items, item2users, test_user2items = dataset_loader.get_dictionaries()
 
 
-ncf = NCF.NeuralCollaborativeFiltering(user_num+1, item_num+1, 64).cuda()
+            ncf = NCF.NeuralCollaborativeFiltering(user_num+1, item_num+1, 16).cuda()
+ncf.join_output_weights()
+print('Hit ratio:', HitRatio())
 
 mlp_optimizer = optim.Adam(list(ncf.mlp_item_embeddings.parameters()) +
                         list(ncf.mlp_user_embeddings.parameters()) +
@@ -58,16 +99,16 @@ mlp_optimizer = optim.Adam(list(ncf.mlp_item_embeddings.parameters()) +
 gmf_optimizer = optim.Adam(list(ncf.gmf_item_embeddings.parameters()) +
                         list(ncf.gmf_user_embeddings.parameters()) +
                         list(ncf.gmf_out.parameters()), lr=1e-3)
-ncf_optimizer = optim.SGD(ncf.parameters(), lr=5e-4)
+ncf_optimizer = optim.Adam(ncf.parameters(), lr=5e-4)
+
 
 print('\nTraining MLP')
-train('mlp', mlp_optimizer)
+train('mlp', mlp_optimizer, epochs=1)
 
 print('\nTrainging GMF')
-train('gmf', gmf_optimizer)
+train('gmf', gmf_optimizer, epochs=1)
 
 ncf.join_output_weights()
 
 print('\nTraining NCF')
-train('ncf', ncf_optimizer)
-
+train('ncf', ncf_optimizer, epochs=20)
